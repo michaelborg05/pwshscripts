331fcd26854d4780cbc1972131912a2d373e4ac8
michaelborg05 <54766713+michaelborg05@users.noreply.github.com>
2022-11-04 14:29:58 +1100

    Update script with new ME APIs and customise for hashverse

--- MonitorNFTFloor.ps1
+++ MonitorNFTFloor.ps1
@@ -1,4 +1,24 @@
-﻿function Get-ObjectMember {
+$AlertPrice= 0.15
+$global:MAxAlerts=4
+$global:AlertCount=0
+
+Function Send-Telegram {
+    Param([Parameter(Mandatory=$true)][String]$Message)
+    $Telegramtoken = "594185771:AAEGBNDncM3455gyutHlwjTlnPIdeRs0dYE"
+    #$Telegramchatid = "416082917"
+    $Telegramchatid = "-773724465"#NFTSniper
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"
+    $global:AlertCount= $global:AlertCount + 1
+    if ($global:AlertCount -ge $global:MaxAlerts) {
+        $Message="3 alerts received in short time. Pausing script for 30mins"
+        $Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"
+        start-sleep 1800
+        $global:AlertCount=0
+    }
+}
+
+function Get-ObjectMember {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
@@ -10,32 +30,81 @@
     }
 }
 
-$run = $true
-while ($run) {
+function CheckFloorPrice() {
     #Get stats
-    $response = Invoke-RestMethod 'https://api-mainnet.magiceden.io/v2/xc/collections/eth/0xb99e4e9b8fd99c2c90ad5382dbc6adfdfe3a33f3/stats' -Method 'GET' -Headers $headers
+    $response = Invoke-RestMethod 'api-mainnet.magiceden.io/v2/xc/collections/eth/0xb99e4e9b8fd99c2c90ad5382dbc6adfdfe3a33f3/stats' -Method 'GET' -Headers $headers
 
     $response  | Get-ObjectMember | foreach {
                  if ($_.key -eq 'floorPrice') {
                     $floorPr = $_.value
                     }
-                 if ($_.key -eq 'listedCount') {
+                 if ($_.key -eq 'totalListedCount') {
                     $listed = $_.value
                     }
 
     }
-    $floorPr = $Floorpr / 1000000000
+    write-host "Floor price before divide: $floorpr"
+    $floorPr = $Floorpr / 1000000000000000000
+
+    if ($floorPr -le $AlertPrice)
+    {
+        $desc = "Hashverse Floor price: " + $floorPr
+        Send-Telegram $desc
+
+    } else {$global:AlertCount=0}
 
     write-host "Listed: $Listed"
     write-host "Floor Price: " $floorPr
+
+}
+
+
+function FindCheapNFTs() {
+    #Get stats
+    $Offset = 0
+    $NFTSBelowAlert=0
+    $Limit = 20
+    $cheapNFTsList = @{}
+    $URI = "api-mainnet.magiceden.io/v2/xc/collections/eth/0xb99e4e9b8fd99c2c90ad5382dbc6adfdfe3a33f3/orders?sort=askAmountNum&limit=20"
+    $response = Invoke-RestMethod $URI -Method 'GET' -Headers $headers
+        
+    $outputpath = "C:\temp\output.txt"
+#    [System.IO.File]::WriteAllLines($outputpath, $response) 
+     $response.items | out-file $outputpath
+    $counter = 0
+    foreach ($i in $response.items) {
+        $i | Get-ObjectMember | foreach {
+                if ($_.key -eq "askAmountNum" -and [decimal]$_.value -le $AlertPrice) {
+                $NFTSBelowAlert= $NFTSBelowAlert + 1
+                $cheapNFTsList[$NFTSBelowAlert] = $_.value  
+                write-host "$NFTSBelowAlert : " $_.value
+                }
+                 
+        }
+    }
+    $text= ""
+    if ($cheapNFTsList.Count -gt 0) {
+        $cheapNFTsList.GetEnumerator() | ForEach-Object {
+            $text = $text + $_.key + " - " + $_.value + " `r`n"
+        }
+        
+        Send-Telegram $text
+    } else {$global:AlertCount=0}
+
+}
+
+
+
+$run = $true
+while ($run) {
+    $Time = Get-Date
+    write-host  $time
+    
+    CheckFloorPrice
+    FindCheapNFTs
     start-sleep 15
 }
-#Get listings
-#$response = Invoke-RestMethod 'api-mainnet.magiceden.dev/v2/collections/defi_land_seeds/listings?offset=0&limit=20' -Method 'GET' -Headers $headers
-#$temp = $response | ConvertTo-Json 
 
 
 
-    #[System.IO.File]::WriteAllLines("c:\temp\json.txt", $temp)
 
-    
