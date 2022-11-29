e87fc93772ea2e7ee094d36e20a53a309c631486
michaelborg05 <54766713+michaelborg05@users.noreply.github.com>
2022-11-29 11:41:28 +1100

    New file for building and running docker

---
+++ run.sh
@@ -0,0 +1,4 @@
+# this is the batch file to build and run the docker image
+
+docker build --no-cache -t linuxpwrsh .
+docker run -d -v "$(pwd):/pwshscripts/config" --name nfts linuxpwrsh pwsh MonitorNFTFloor.ps1
