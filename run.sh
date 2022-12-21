8bd9b2fc44c751ef6710d5d77511dff3671b5e3d
michaelborg05 <54766713+michaelborg05@users.noreply.github.com>
2022-12-21 13:57:23 +1100

    add force remove of nft container before creating new one

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # this is the batch file to build and run the docker image
 
 docker build --no-cache -t linuxpwrsh .
+docker rm nfts -f
 docker run -d -v "$(pwd):/pwshscripts/config" --name nfts linuxpwrsh pwsh MonitorNFTFloor.ps1
