2651b950daa9bd997e6289a67154a8d6563bc618
michaelborg05 <54766713+michaelborg05@users.noreply.github.com>
2022-12-26 20:32:14 +1100

    added force option to prune commands so it wont prompt

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # this is the batch file to build and run the docker image
 
 docker stop nfts
-docker container prune
-docker image prune -a
+docker container prune -f
+docker image prune -a -f
 docker build --no-cache -t linuxpwrsh .
 docker rm nfts -f
 docker run -d -v "$(pwd):/pwshscripts/config" --name nfts linuxpwrsh pwsh MonitorNFTFloor.ps1
